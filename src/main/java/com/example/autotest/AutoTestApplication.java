9cb8583f3891f78cb9d2f0825e4fd577a50bdc25
HyungCheol Kim <hcgoon@gmail.com>
2017-11-15 19:32:10 +0900

    Initial commit

---
+++ src/main/java/com/example/autotest/AutoTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.autotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestApplication.class, args);
+	}
+}
