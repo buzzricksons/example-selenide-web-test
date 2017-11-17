b5fdde763a168cfd4f8f59ecee801fb053d2e3e0
HyungCheol Kim <hcgoon@gmail.com>
2017-11-17 12:24:17 +0900

    add getResult from index method

--- src/main/java/com/example/page/GoogleResultsPage.java
+++ src/main/java/com/example/page/GoogleResultsPage.java
@@ -1,8 +1,11 @@
 package com.example.page;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class GoogleResultsPage {
     @FindBy(how = How.CSS, using = "#ires .g")
@@ -10,5 +13,9 @@ public class GoogleResultsPage {
 
     public ElementsCollection results() {
         return results;
+    }
+
+    public SelenideElement getResult(int index) {
+        return $("#ires .g", index);
     }
 }
