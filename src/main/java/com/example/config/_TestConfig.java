2ad9a99fe24e81b3ba00da2ffb900ac38c3edbe5
Hyungcheol Kim <hcgoon@gmail.com>
2017-12-05 17:23:21 +0900

    add component

--- src/main/java/com/example/config/_TestConfig.java
+++ src/main/java/com/example/config/_TestConfig.java
@@ -3,8 +3,10 @@ package com.example.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 @Configuration
 public class _TestConfig {
 
