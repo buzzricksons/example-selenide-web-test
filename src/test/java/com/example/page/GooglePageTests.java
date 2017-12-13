6f36d5d5fbaee545b7f952b8c571cb14eaafe39b
HyungCheol Kim <hcgoon@gmail.com>
2017-12-13 16:09:57 +0900

    add custom screenshot save folder

--- src/test/java/com/example/page/GooglePageTests.java
+++ src/test/java/com/example/page/GooglePageTests.java
@@ -1,12 +1,14 @@
 package com.example.page;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
@@ -18,20 +20,26 @@ import static com.codeborne.selenide.Selenide.open;
 public class GooglePageTests {
     @Value("${setting.browser}")
     String browser;
+    @Value("${setting.screenshot-folder}")
+    String screenshotFolder;
 
     @Test
     public void testGoogleSearch() {
-        System.setProperty("selenide.browser", browser);//Set Chrome
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+
+        System.setProperty("selenide.browser", browser);
+        System.setProperty("selenide.reportsFolder", screenshotFolder + now.format(formatter));
+
+
         GoogleSearchPage searchPage = open("http://google.com/", GoogleSearchPage.class);
         GoogleResultsPage resultsPage = searchPage.search("selenide");
 
         resultsPage.results().get(0).shouldHave(text("Selenide: concise UI tests in Java"));
 
-        //Default JUnit
-        Assert.assertEquals(10, resultsPage.results().size());
-
-        //Selenide
-        resultsPage.results().shouldHave(size(10));
+//        Assert.assertEquals(10, resultsPage.results().size());//Default JUnit
+        resultsPage.results().shouldHave(size(9));
 
     }
 }
