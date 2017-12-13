13e25ee04f8ea2e5cc9569062433c6f0660ce5d0
HyungCheol Kim <hcgoon@gmail.com>
2017-12-13 14:47:38 +0900

    add get application.yml value

--- src/test/java/com/example/page/GooglePageTests.java
+++ src/test/java/com/example/page/GooglePageTests.java
@@ -2,15 +2,26 @@ package com.example.page;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+
+@SpringBootTest
+@SpringBootConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 public class GooglePageTests {
+    @Value("${setting.browser}")
+    String browser;
 
     @Test
     public void testGoogleSearch() {
-        System.setProperty("selenide.browser", "Chrome");//Set Chrome
+        System.setProperty("selenide.browser", browser);//Set Chrome
         GoogleSearchPage searchPage = open("http://google.com/", GoogleSearchPage.class);
         GoogleResultsPage resultsPage = searchPage.search("selenide");
 
