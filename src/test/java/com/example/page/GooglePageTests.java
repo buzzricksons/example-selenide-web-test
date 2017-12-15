acac34fec03b907581ac71d61b704d5e9b968c74
HyungCheol Kim <hcgoon@gmail.com>
2017-12-15 16:49:08 +0900

    remove unused import

--- src/test/java/com/example/page/GooglePageTests.java
+++ src/test/java/com/example/page/GooglePageTests.java
@@ -1,11 +1,7 @@
 package com.example.page;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
