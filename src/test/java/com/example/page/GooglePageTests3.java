7bfbe3906921b4c01e05d37e808bd95b2a369105
HyungCheol Kim <hcgoon@gmail.com>
2017-12-27 17:36:55 +0900

    add test class

---
+++ src/test/java/com/example/page/GooglePageTests3.java
@@ -0,0 +1,42 @@
+package com.example.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.example.AbstractTests;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ * Test for Google Search.
+ *
+ * @author HyungCheol Kim
+ *
+ */
+public class GooglePageTests3 extends AbstractTests {
+    /**
+     * Test for keyword "selenide".
+     *
+     */
+    @Test
+    public void testGoogleSearch() {
+        GoogleSearchPage searchPage = open("http://google.co.jp/", GoogleSearchPage.class);
+        GoogleResultsPage resultsPage = searchPage.search("selenide");
+
+        ElementsCollection result = resultsPage.results();
+
+        result.get(0).shouldHave(text("Selenide: concise UI tests in Java"));
+        result.get(1).shouldHave(text("Selenide入門 - Qiita"));
+        result.get(1).$$(".rc a").get(0).shouldHave(text("Selenide入門 - Qiita"));
+        result.shouldHave(size(10));
+
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println(result.get(1).innerHtml());
+        System.out.println("--------------------------------------------------------------------------------------");
+//        System.out.println(result.find(By.className("rc")).innerHtml());
+
+    }
+}
