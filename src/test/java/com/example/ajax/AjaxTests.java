cabe4c3c056dc436a1c6e375c0187c327cf8c90f
HyungCheol Kim <hcgoon@gmail.com>
2018-03-01 19:51:09 +0900

    add ajax test and remove daum test

---
+++ src/test/java/com/example/ajax/AjaxTests.java
@@ -0,0 +1,19 @@
+package com.example.ajax;
+
+import com.example.AbstractTests;
+import com.example.Settings;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AjaxTests extends AbstractTests {
+    @Autowired
+    private Settings settings;
+
+    /**
+     * Test for Ajax.
+     *
+     */
+    @Test
+    public void testAjax() {
+    }
+}
