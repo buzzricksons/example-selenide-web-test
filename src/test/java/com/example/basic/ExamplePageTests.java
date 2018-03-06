dcbe30af835af0a2af61502cf3346dc2c09891e0
HyungCheol Kim <hcgoon@gmail.com>
2018-03-06 19:36:03 +0900

    edit basic example

--- src/test/java/com/example/basic/ExamplePageTests.java
+++ src/test/java/com/example/basic/ExamplePageTests.java
@@ -2,17 +2,12 @@ package com.example.basic;
 
 import com.example.AbstractTests;
 import com.example.Settings;
-import com.example.page.google.GoogleResultsPage;
-import com.example.page.google.GoogleSearchPage;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 /**
  * Test for Example Search.
@@ -32,6 +27,6 @@ public class ExamplePageTests extends AbstractTests {
     public void testExampleSearch() {
         open(settings.getGooglePage());
         $(By.name("q")).val("selenide").pressEnter();
-        $$("#ires .g").get(0).shouldHave(text("Selenide: concise UI tests in Java"));
+        $("#ires .g").shouldHave(text("Selenide: concise UI tests in Java"));
     }
 }
