8e5f5c8b22899b396bf37e896b8027910f692ab2
HyungCheol Kim <hcgoon@gmail.com>
2018-03-14 19:24:52 +0900

    add javadoc

--- src/test/java/com/example/Settings.java
+++ src/test/java/com/example/Settings.java
@@ -9,12 +9,22 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class Settings {
+    /**
+     * When test failed, schreenshot and html file saved in this folder.
+     */
     @Value("${setting.screenshot-folder}")
     private String screenshotFolder;
 
+    /**
+     * If you use HeadlessChrome then set true.
+     * ※Not yet supported.
+     */
     @Value("${setting.isHeadless}")
     private String isHeadless;
 
+    /**
+     * Set time interval for each test class ended.
+     */
     @Value("${setting.timeInterval}")
     private String timeInterval;
 
