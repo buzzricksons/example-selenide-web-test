de99e85d33c6bbaa94e78e08a7aada72a1d30220
HyungCheol Kim <hcgoon@gmail.com>
2018-03-14 19:17:25 +0900

    move another package

--- src/main/java/com/example/Controller/ExamWebAppController.java
+++ src/main/java/com/example/Controller/ExamWebAppController.java
@@ -1,4 +1,4 @@
-package com.example.page.ajax;
+package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/Controller/ExamWebAppRestController.java
+++ src/main/java/com/example/Controller/ExamWebAppRestController.java
@@ -1,4 +1,4 @@
-package com.example.page.ajax;
+package com.example.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
